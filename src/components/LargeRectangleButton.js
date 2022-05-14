d4e13af8c9b192b11298e1c8ddaa1f89ada12f14
Tomm Jobit <tjobit@macbook-air-de-tomm.home>
2022-05-14 10:34:06 +0200

    ci: position and state on input

--- src/components/LargeRectangleButton.js
+++ src/components/LargeRectangleButton.js
@@ -5,10 +5,10 @@ import Styles, { Colors, Fonts } from '../styles/Styles';
 import Svg1 from '../assets/svgs/rectangle_button_1.svg';
 import Svg2 from '../assets/svgs/rectangle_button_2.svg';
 
-const LargeRectangleButton = ({ onPress, buttonText, style }) => (
-  <TouchableOpacity onPress={onPress} style={{ ...styles.container, ...style }} >
+const LargeRectangleButton = ({ onPress, buttonText, style, disabled }) => (
+  <TouchableOpacity disabled={disabled} onPress={onPress} style={{ ...styles.container, ...style }} >
     <Svg1 width={'100%'} height={'100%'} style={{ ...styles.svg }} />
-    <Svg2 width={'100%'} height={'100%'} style={{ ...styles.svg }} />
+    <Svg2 width={'100%'} height={'100%'} style={{ ...styles.svg, left: '-17%' }} />
     <Text style={{ ...Fonts.bold(34, Colors.white), letterSpacing: 12 }}>{buttonText}</Text>
   </TouchableOpacity>
 );
--- src/screens/AuthenticationScreen.js
+++ src/screens/AuthenticationScreen.js
@@ -1,12 +1,6 @@
-import React from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  TextInput,
-  SafeAreaView,
-  Button
-} from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TextInput, SafeAreaView } from 'react-native';
+import { responsiveHeight } from 'react-native-responsive-dimensions';
 import { Colors, Fonts } from '../styles/Styles';
 import LargeRectangleButton from '../components/LargeRectangleButton';
 import Svg1 from '../assets/svgs/background_auth_1.svg';
@@ -14,8 +8,9 @@ import Svg2 from '../assets/svgs/background_auth_2.svg';
 import Svg3 from '../assets/svgs/background_auth_3.svg';
 
 const AuthenticationScreen = ({ navigation }) => {
-  return (
+  const [name, setNameValue] = useState('');
 
+  return (
     <View style={styles.container}>
       <View>
         <Svg2 height={500} width={450} style={{ ...styles.svg }} />
@@ -24,11 +19,11 @@ const AuthenticationScreen = ({ navigation }) => {
       </View>
       <SafeAreaView style={styles.content} >
         <Text style={{ ...Fonts.bold(50, Colors.white) }}>Dropy</Text>
-        <TextInput style={{ ...Fonts.bold(15, Colors.purple5), ...styles.textInput }} placeholder="What's your name ?" placeholderTextColor={Colors.purple5} />
-        <LargeRectangleButton style={{ width: 228, height: 57 }} buttonText={'start'} onPress={() => navigation.navigate('Home')} />
+        <TextInput onChange={(text) => setNameValue(text)} style={{ ...Fonts.bold(15, Colors.purple5), ...styles.textInput }} placeholder="What's your name ?" placeholderTextColor={Colors.purple5} />
+        <LargeRectangleButton disabled={name.length === 0} style={styles.largeButton} buttonText={'start'} onPress={() => navigation.navigate('Home')} />
       </SafeAreaView>
     </View>
-  );
+  )
 };
 
 export default AuthenticationScreen;
@@ -40,14 +35,21 @@ const styles = StyleSheet.create({
   content: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-evenly'
+    justifyContent: 'space-around'
   },
   textInput: {
+    marginBottom: responsiveHeight(20),
     width: 267,
     height: 45,
     borderRadius: 14,
     padding: 15,
     backgroundColor: Colors.lighterGrey
+  },
+  largeButton: {
+    position: 'absolute',
+    bottom: responsiveHeight(7),
+    width: 228,
+    height: 57
   },
   svg: {
     position: 'absolute'
