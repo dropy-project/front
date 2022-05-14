dccdffced0b84ec035003792518f103cbed7ec15
Guillaume <froidcourt.guillaume.etu@gmail.com>
2022-05-14 10:24:18 +0200

    feat: add map

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,195 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, View, Text } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import Styles, { Colors, Fonts } from '../styles/Styles';
 import HomeScreenTabBar from '../components/HomeScreenTabBar';
 import Map from '../assets/svgs/map';
+
+const mapStyle = [
+  {
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#f5f5f5'
+      }
+    ]
+  },
+  {
+    'elementType': 'labels.icon',
+    'stylers': [
+      {
+        'visibility': 'off'
+      }
+    ]
+  },
+  {
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#616161'
+      }
+    ]
+  },
+  {
+    'elementType': 'labels.text.stroke',
+    'stylers': [
+      {
+        'color': '#f5f5f5'
+      }
+    ]
+  },
+  {
+    'featureType': 'administrative.land_parcel',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#bdbdbd'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#eeeeee'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#757575'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi.park',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#e5e5e5'
+      }
+    ]
+  },
+  {
+    'featureType': 'poi.park',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#9e9e9e'
+      }
+    ]
+  },
+  {
+    'featureType': 'road',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#ffffff'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.arterial',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#757575'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.highway',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#dadada'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.highway',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#616161'
+      }
+    ]
+  },
+  {
+    'featureType': 'road.local',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#9e9e9e'
+      }
+    ]
+  },
+  {
+    'featureType': 'transit.line',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#e5e5e5'
+      }
+    ]
+  },
+  {
+    'featureType': 'transit.station',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#eeeeee'
+      }
+    ]
+  },
+  {
+    'featureType': 'water',
+    'elementType': 'geometry',
+    'stylers': [
+      {
+        'color': '#c9c9c9'
+      }
+    ]
+  },
+  {
+    'featureType': 'water',
+    'elementType': 'geometry.fill',
+    'stylers': [
+      {
+        'color': '#83a5a9'
+      }
+    ]
+  },
+  {
+    'featureType': 'water',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {
+        'color': '#9e9e9e'
+      }
+    ]
+  }
+];
 
 const HomeScreen = ({ navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Map style={StyleSheet.absoluteFillObject} width={'100%'} />
+      <MapView
+        // provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        }}
+        // customMapStyle={mapStyle}
+        style={StyleSheet.absoluteFillObject}
+        width={'100%'} />
       <SafeAreaView style={styles.safeAreaView}>
         <View style={styles.textContainer}>
           <Text style={{ ...Fonts.ligth(18, Colors.primary) }}>Je suis un texte light</Text>
