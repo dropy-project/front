c92637700cd5a17f203ca7782ae737918f7aee0c
Guillaume <93042538+gfroidcourt@users.noreply.github.com>
2022-06-28 10:05:02 +0200

    Android multiple fixes (#73)
    * no longer screen rotation and POI hides
    
    * Drop works. TODO: pick up marker
    
    * Different map style for IOS and for Android
    
    Co-authored-by: celian-rib <celian.riboulet@gmail.com>

--- src/components/ConfirmDropOverlay.js
+++ src/components/ConfirmDropOverlay.js
@@ -57,9 +57,7 @@ const ConfirmDropOverlay = ({ visible = false, onCloseOverlay: closeOverlay = ()
   const sendDrop = async () => {
     try {
       // Haptic.trigger('impactMedium');
-
       const dropy = await API.createDropy(user.id, userCoordinates.latitude, userCoordinates.longitude);
-
       if(mediaIsFile(dropyCreateParams.mediaType)) {
         const mediaResult = await API.postDropyMediaFromPath(dropy.id, dropyCreateParams.dropyFilePath, dropyCreateParams.mediaType);
         console.log('[File upload] API response', mediaResult.data);
@@ -97,7 +95,7 @@ const ConfirmDropOverlay = ({ visible = false, onCloseOverlay: closeOverlay = ()
             <Animated.View style={{ ...styles.dropyPreviewContainer, transform: [{ scale : animatedScale }] }}>
               {dropyCreateParams.dropyFilePath != null ? (
                 <>
-                  <Image source={{ uri: dropyCreateParams.dropyFilePath }} style={styles.previewImage} />
+                  <Image source={{ uri: dropyCreateParams.dropyFilePath }} style={styles.previewImage}/>
                   <View style={styles.previewImageOverlay} />
                   <Ionicons name="ios-camera-outline" size={80} color={Colors.white} style={styles.cameraIcon}/>
                 </>
