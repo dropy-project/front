3d7774e771e151e0ecf7fae9ea93cefb94f55f54
celian-rib <celian.riboulet@gmail.com>
2022-07-11 16:22:41 +0200

    Fix JSON

--- src/utils/encrypt.js
+++ src/utils/encrypt.js
@@ -1,11 +1,9 @@
 import crypto from 'crypto-js';
-import { SECRET_KEY } from '@env';
 
 export const encryptMessage = (text) => {
-  return crypto.AES.encrypt(JSON.stringify({ text }), SECRET_KEY).toString();
+  return crypto.AES.encrypt(text, 'bite').toString();
 };
 
 export const decryptMessage = (text) => {
-  const info2 = crypto.AES.decrypt(text, SECRET_KEY).toString(crypto.enc.Utf8);
-  return JSON.parse(info2).text;
+  return crypto.AES.decrypt(text, 'bite').toString(crypto.enc.Utf8);
 };
