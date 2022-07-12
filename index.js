db6fe38fb627c9222b8e8d875c6d2be4adb9aedd
Guillaume <guillaume.froidcourt@etu.u-bordeaux.fr>
2022-07-12 12:28:52 +0200

    setup dev environment

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ import { name as appName } from './app.json';
 
 Sentry.init({
   dsn: 'https://19407e7c32a2487689649a399a55c564@o1315355.ingest.sentry.io/6567185',
+  // eslint-disable-next-line no-undef
+  environment: __DEV__ ? 'dev' : 'production',
+
   // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
   // We recommend adjusting this value in production.
   tracesSampleRate: 1.0,
