9299418485373bb831fa5898b55e80c1bcfd4d9d
celian-rib <celian.riboulet@gmail.com>
2022-07-12 11:25:17 +0200

    Fix lets chat button

--- src/screens/DisplayDropyMediaScreen.js
+++ src/screens/DisplayDropyMediaScreen.js
@@ -1,36 +1,26 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet } from 'react-native';
+import { Colors } from '../styles/Styles';
+
 import FooterConfirmation from '../components/FooterConfirmation';
 import GoBackHeader from '../components/GoBackHeader';
-
-import { Colors } from '../styles/Styles';
 import DropyMediaViewer from '../components/DropyMediaViewer';
 import useOverlay from '../hooks/useOverlay';
-import API from '../services/API';
-import { messageTimeString } from '../utils/time';
+import useConversationSocket from '../hooks/useConversationSocket';
 
 const DisplayDropyMediaScreen = ({ navigation, route }) => {
   const { dropy } = route.params || {};
 
   const { sendAlert } = useOverlay();
-
-  const getConversations = async () => {
-    const result = await API.getConversations();
-
-    const datedConversations = result.data.map((conversation) => ({
-      ...conversation,
-      lastMessageDate: messageTimeString(conversation.lastMessageDate),
-    }));
-
-    return datedConversations;
-  };
+  const { conversations } = useConversationSocket();
 
   const openChat = async () => {
     try {
-      const convs = await getConversations();
-      navigation.navigate('Chat', { conversation: convs.find(c => c.id === dropy.conversationId) });
+      navigation.navigate('Chat', {
+        conversation: conversations.find(c => c.id === dropy.conversationId),
+      });
     } catch (error) {
-      console.log(error?.response?.data ?? error);
+      console.log('Open chat error', error?.response?.data ?? error);
       sendAlert({
         title: 'Oh that\'s bad...',
         description: 'Looks like we can\'t load your conversations right now...',
