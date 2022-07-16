cfc3981fdd014dfae56fc34544b38aa5565f1cb4
celian-rib <celian.riboulet@gmail.com>
2022-07-16 19:08:05 +0200

    Disable sentry when app is in background

--- index.js
+++ index.js
@@ -2,19 +2,21 @@
  * @format
  */
 
-import { AppRegistry } from 'react-native';
+import { AppRegistry, AppState } from 'react-native';
 import * as Sentry from '@sentry/react-native';
 import App from './App';
 import { name as appName } from './app.json';
 
-Sentry.init({
-  dsn: 'https://19407e7c32a2487689649a399a55c564@o1315355.ingest.sentry.io/6567185',
-  // eslint-disable-next-line no-undef
-  environment: __DEV__ ? 'dev' : 'production',
+if(AppState.currentState !== 'background') {
+  Sentry.init({
+    dsn: 'https://19407e7c32a2487689649a399a55c564@o1315355.ingest.sentry.io/6567185',
+    // eslint-disable-next-line no-undef
+    environment: __DEV__ ? 'dev' : 'production',
 
-  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-  // We recommend adjusting this value in production.
-  tracesSampleRate: 1.0,
-});
+    // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+    // We recommend adjusting this value in production.
+    tracesSampleRate: 1.0,
+  });
+}
 
 AppRegistry.registerComponent(appName, () => App);
