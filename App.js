67ca2e79aa4e47e08b6dafce98f2026034b62739
celian-rib <celian.riboulet@gmail.com>
2022-07-20 18:49:20 +0200

    Use network properties & Fix sentry auth token

--- App.js
+++ App.js
@@ -10,6 +10,8 @@ import {
   SpaceGrotesk_700Bold
 } from '@expo-google-fonts/space-grotesk';
 
+import * as Sentry from '@sentry/react-native';
+
 import Navigation from './src/navigation/Navigation';
 import UserProvider from './src/states/UserContextProvider';
 import GeolocationProvider from './src/states/GeolocationContextProvider';
@@ -17,6 +19,17 @@ import BackgroundGolocationContextProvider from './src/states/BackgroundGolocati
 import NotificationProvider from './src/states/NotificationProvider';
 import OverlayContextProvider from './src/states/OverlayContextProvider';
 import SocketProvider from './src/states/SocketContextProvider';
+
+
+Sentry.init({
+  dsn: 'https://19407e7c32a2487689649a399a55c564@o1315355.ingest.sentry.io/6567185',
+  // eslint-disable-next-line no-undef
+  environment: __DEV__ ? 'dev' : 'production',
+
+  // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+  // We recommend adjusting this value in production.
+  tracesSampleRate: 1.0,
+});
 
 export default function App() {
 
