9b4b05cdd11518ff70d2046716040675aac04b7c
Guillaume <guillaume.froidcourt@etu.u-bordeaux.fr>
2022-07-22 16:51:46 +0200

    add time constants

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,16 +1,20 @@
-const ONE_DAY = 24 * 60 * 60 * 1000;
+const ONE_SECOND = 1000;
+const ONE_MINUTE = 60 * ONE_SECOND;
+const ONE_HOUR = 60 * ONE_MINUTE;
+const ONE_DAY = 24 * ONE_HOUR;
+const ONE_MONTH = 30 * ONE_DAY;
 
 export const createDropTimeString = (dropLifeTime) => {
-  if (dropLifeTime < 60000) {
-    return `${Math.floor(dropLifeTime / 1000)}s`;
-  } else if (dropLifeTime < 3600000) {
-    return `${Math.floor(dropLifeTime / 60000)}m`;
-  } else if (dropLifeTime < 86400000) {
-    return `${Math.floor(dropLifeTime / 3600000)}h`;
-  } else if (dropLifeTime < 31536000000) {
-    return `${Math.floor(dropLifeTime / 86400000)}d`;
+  if (dropLifeTime < ONE_MINUTE) {
+    return `${Math.floor(dropLifeTime / ONE_SECOND)}s`;
+  } else if (dropLifeTime < ONE_HOUR) {
+    return `${Math.floor(dropLifeTime / ONE_MINUTE)}m`;
+  } else if (dropLifeTime < ONE_DAY) {
+    return `${Math.floor(dropLifeTime / ONE_HOUR)}h`;
+  } else if (dropLifeTime < ONE_MONTH) {
+    return `${Math.floor(dropLifeTime / ONE_DAY)}d`;
   } else {
-    return `${Math.floor(dropLifeTime / 31536000000)}y`;
+    return `${Math.floor(dropLifeTime / ONE_MONTH)}y`;
   }
 };
 
