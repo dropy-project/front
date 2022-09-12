473d3155f1299812b0f33b3b32d6533906fc790e
Célian Riboulet <celian.riboulet@gmail.com>
2022-09-12 21:58:38 +0200

    Update src/screens/Splash.js

--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -38,7 +38,6 @@ const Splash = ({ navigation }) => {
   useEffectForegroundOnly(() => {
     API.serverVersionIsCompatible().then(compatible => {
       if (compatible) {
-        // login();
         navigation.reset({ index: 0, routes: [{ name: 'Onboarding' }] });
       } else {
         sendAlert({
