635388a5a324a763430f5116d38a1b7408f604e1
Célian Riboulet <celian.riboulet@gmail.com>
2022-09-27 14:58:19 +0200

    Update src/components/AnimatedFlask.js

--- src/components/AnimatedFlask.js
+++ src/components/AnimatedFlask.js
@@ -64,7 +64,6 @@ const AnimatedFlask = ({ color = Colors.purple2, size = 60 }) => {
   const animateFill = () => {
     if (user.lastEnergyIncrement == null) return;
     const energyClamped = Math.min(Math.max((user.energy * 100 / MAX_ENERGY), 0), 100);
-    console.log('ANIMATE FILL', energyClamped);
 
     const anim = Animated.sequence([
       Animated.delay(500),
