666b1f5d754b005dee4141bceb093840ddb1fd53
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-11 10:30:47 +0100

    feat: added boxWidth state

--- src/components/map/RetrievedDropyMapMarker.js
+++ src/components/map/RetrievedDropyMapMarker.js
@@ -1,24 +1,43 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Marker } from 'react-native-maps';
 import Styles, { Colors } from '../../styles/Styles';
 import DropyMediaViewer from '../other/DropyMediaViewer';
+import MEDIA_TYPES from '../../utils/mediaTypes';
 
-const RetrievedDropyMapMarker = ({ dropy, onPress }) => (
-  <Marker
-    coordinate={{ latitude: dropy.latitude, longitude: dropy.longitude }}
-    onPress={onPress}
-    tracksViewChanges={true}
-  >
-    <View style={styles.container}>
-      <View style={styles.visibleContainer}>
-        <View style={styles.mediaContainer}>
-          <DropyMediaViewer dropy={dropy} />
+const RetrievedDropyMapMarker = ({ dropy, onPress }) => {
+  const [boxWidth, setBoxWidth] = useState(null);
+
+  useEffect(() => {
+    switch (dropy.mediaType) {
+      case MEDIA_TYPES.PICTURE:
+        setBoxWidth(100);
+        break;
+      case MEDIA_TYPES.TEXT:
+        setBoxWidth(200);
+        break;
+      default:
+        break;
+    }
+  }, []);
+
+  return (
+
+    <Marker
+      coordinate={{ latitude: dropy.latitude, longitude: dropy.longitude }}
+      onPress={onPress}
+      tracksViewChanges={true}
+    >
+      <View style={styles.container}>
+        <View style={styles.visibleContainer}>
+          <View style={styles.mediaContainer}>
+            <DropyMediaViewer dropy={dropy} />
+          </View>
         </View>
       </View>
-    </View>
-  </Marker>
-);
+    </Marker>
+  );
+};
 
 // eslint-disable-next-line react/display-name
 export default RetrievedDropyMapMarker;
