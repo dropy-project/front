7071335de6a81bde17a18a2e1b9018f7973fa940
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-11 17:19:57 +0100

    fix: maxHeight change when dropyText is into DropyMapMarker

--- src/components/other/DropyMediaViewer.js
+++ src/components/other/DropyMediaViewer.js
@@ -72,7 +72,7 @@ const DropyMediaViewer = ({ dropy, isIntoDropyMapMarker, style = StyleSheet.abso
 
   if (dropy.mediaType === MEDIA_TYPES.TEXT) {
     return (
-      <ScrollView style={{ ...style, maxHeight: 300 }} contentContainerStyle={styles.textContentContainer}>
+      <ScrollView style={{ ...style, maxHeight: (isIntoDropyMapMarker ? 300 : null) }} contentContainerStyle={styles.textContentContainer}>
         {loading && (
           <View style={{ ...StyleSheet.absoluteFillObject, ...Styles.center }}>
             <LoadingSpinner />
