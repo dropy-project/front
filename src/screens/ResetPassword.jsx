ec0c42acb4381ded9546e8d76973cad274917b20
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-13 11:16:59 +0100

    feat: added goBackHeader

--- src/screens/ResetPassword.jsx
+++ src/screens/ResetPassword.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
+import GoBackHeader from '../components/other/GoBackHeader';
+import { responsiveWidth } from 'react-native-responsive-dimensions';
+import Styles, { Colors, Fonts } from '../styles/Styles';
 
 const ResetPassword = () => (
-  <View>
-    <Text>ResetPassword</Text>
-  </View>
+  <SafeAreaView style={styles.container}>
+    <GoBackHeader text='Reset Password' />
+    <View style={styles.content}>
+    </View>
+  </SafeAreaView>
 );
 
 export default ResetPassword;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: Colors.white,
+    ...Styles.safeAreaView,
+  },
+  content: {
+    width: responsiveWidth(100),
+    alignItems: 'center',
+    marginTop: 20,
+    height: '80%',
+  },
+});
