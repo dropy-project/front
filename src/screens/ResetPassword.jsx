b1412b642db3786eb4ff71c3e8eadd271a151c99
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-13 12:17:06 +0100

    feat: finish design

--- src/screens/ResetPassword.jsx
+++ src/screens/ResetPassword.jsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { responsiveWidth } from 'react-native-responsive-dimensions';
 import { SimpleLineIcons } from '@expo/vector-icons';
 import GoBackHeader from '../components/other/GoBackHeader';
-import Styles, { Colors } from '../styles/Styles';
+import Styles, { Colors, Fonts } from '../styles/Styles';
 import FormInput from '../components/input/FormInput';
 import GlassButton from '../components/input/GlassButton';
 
@@ -12,21 +12,21 @@ const ResetPassword = ({ navigation }) => (
   <SafeAreaView style={styles.container}>
     <GoBackHeader text='Reset Password' />
     <View style={styles.content}>
-      <View style={styles.icon}>
-        <SimpleLineIcons name='key' size={74} color={Colors.purple1} />
-      </View>
       <View style={styles.formBox}>
-        <Text>Réinitialiser son mot de passe</Text>
-        <FormInput
-        //ref={emailInputRef}
-        //onEdited={setEmail}
-          placeholder='Email'
-          inputStyle={{ backgroundColor: Colors.lighterGrey }}
-          isEmail
-          //defaultValue={email}
-          autoComplete='email'
-        />
-        <Text>Un lien permettant de créer ton nouveau mot de passe te sera ensuite envoyé sur ton adresse email.</Text>
+        <SimpleLineIcons name='key' size={74} color={Colors.purple1} style={{ transform: [{ rotate: '180deg' }] }} />
+        <View style={styles.form}>
+          <Text style={styles.title}>Réinitialiser son mot de passe</Text>
+          <FormInput
+            //ref={emailInputRef}
+            //onEdited={setEmail}
+            placeholder='Email'
+            inputStyle={{ backgroundColor: Colors.lighterGrey }}
+            isEmail
+            //defaultValue={email}
+            autoComplete='email'
+          />
+        </View>
+        <Text style={styles.description}>Un lien permettant de créer ton nouveau mot de passe te sera ensuite envoyé sur ton adresse email.</Text>
         <GlassButton buttonText='Envoyer' style={styles.backButton} onPress={() => navigation.goBack()} fontSize={17} />
       </View>
     </View>
@@ -45,19 +45,36 @@ const styles = StyleSheet.create({
   content: {
     width: responsiveWidth(100),
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'space-around',
     marginTop: 20,
     height: '90%',
-    backgroundColor: Colors.lightGrey,
-  },
-  icon: {
-    backgroundColor: Colors.red,
   },
   formBox: {
     justifyContent: 'space-between',
+    alignItems: 'center',
     width: '80%',
-    height: '60%',
+    height: '80%',
     margin: 30,
-    backgroundColor: Colors.green,
+  },
+  form: {
+    width: '100%',
+    alignItems: 'center',
+    height: '20%',
+  },
+  title: {
+    ...Fonts.bold(14, Colors.grey),
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  description: {
+    width: '70%',
+    textAlign: 'center',
+    lineHeight: 14,
+    ...Fonts.regular(12, Colors.grey),
+  },
+  backButton: {
+    width: '50%',
+    height: 50,
+    borderRadius: 20,
   },
 });
