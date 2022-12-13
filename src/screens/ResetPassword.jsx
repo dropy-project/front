075d650792689f58eee35c6d3499960f975f755e
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-13 11:37:09 +0100

    feat: added page content

--- src/screens/ResetPassword.jsx
+++ src/screens/ResetPassword.jsx
@@ -3,12 +3,26 @@ import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
 import GoBackHeader from '../components/other/GoBackHeader';
 import { responsiveWidth } from 'react-native-responsive-dimensions';
 import Styles, { Colors, Fonts } from '../styles/Styles';
+import FormInput from '../components/input/FormInput';
+import GlassButton from '../components/input/GlassButton';
 
 const ResetPassword = () => (
   <SafeAreaView style={styles.container}>
     <GoBackHeader text='Reset Password' />
     <View style={styles.content}>
-    </View>
+      <Text>Réinitialiser son mot de passe</Text>
+      <FormInput
+              //ref={emailInputRef}
+              //onEdited={setEmail}
+              placeholder='Email'
+              inputStyle={{ backgroundColor: Colors.lighterGrey }}
+              isEmail
+              //defaultValue={email}
+              autoComplete='email'
+            />
+      </View>
+      <Text>Un lien permettant de créer ton nouveau mot de passe te sera ensuite envoyé sur ton adresse email.</Text>
+      <GlassButton buttonText='Envoyer' style={styles.backButton} onPress={() => navigation.goBack()} fontSize={17} />
   </SafeAreaView>
 );
 
